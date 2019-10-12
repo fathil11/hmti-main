37a421b768d8d8168fee0152cfd3f745e553075e
fathil11 <fathil.arham@gmail.com>
2019-10-13 03:22:39 +0700

    Fixing Favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,9 +15,9 @@
     <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('img/favicon/apple-icon-152x152.png') }}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/favicon/apple-icon-180x180.png') }}">
     <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('img/favicon/android-icon-192x192.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicon/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('img/favicon/favicon-96x96.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon/favicon-16x16.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicon.png') }}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('img/favicon.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon.png') }}">
     <link rel="manifest" href="{{ asset('img/favicon/manifest.json') }}">
     {{-- <meta name="msapplication-TileColor" content="#ffffff"> --}}
     <meta name="msapplication-TileImage" content="{{ asset('img/favicon/ms-icon-144x144.png') }}">
