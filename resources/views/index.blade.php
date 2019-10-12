68902928305e7005aa5e53fbd05ddd51d2f4366b
fathil11 <fathil.arham@gmail.com>
2019-10-13 03:28:44 +0700

    Try Fixing Favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <!-- Mobile Specific Meta -->
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Favicon-->
-    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('img/favicon/apple-icon-57x57.png') }}">
+    {{-- <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('img/favicon/apple-icon-57x57.png') }}">
     <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('img/favicon/apple-icon-60x60.png') }}">
     <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('img/favicon/apple-icon-72x72.png') }}">
     <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('img/favicon/apple-icon-76x76.png') }}">
@@ -14,13 +14,13 @@
     <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('img/favicon/apple-icon-144x144.png') }}">
     <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('img/favicon/apple-icon-152x152.png') }}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/favicon/apple-icon-180x180.png') }}">
-    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('img/favicon/android-icon-192x192.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicon.png') }}">
-    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('img/favicon.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon.png') }}">
-    <link rel="manifest" href="{{ asset('img/favicon/manifest.json') }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('img/favicon/android-icon-192x192.png') }}"> --}}
+    <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}">
+    {{-- <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('img/favicon.png') }}"> --}}
+    {{-- <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon.png') }}"> --}}
+    {{-- <link rel="manifest" href="{{ asset('img/favicon/manifest.json') }}"> --}}
     {{-- <meta name="msapplication-TileColor" content="#ffffff"> --}}
-    <meta name="msapplication-TileImage" content="{{ asset('img/favicon.png') }}">
+    {{-- <meta name="msapplication-TileImage" content="{{ asset('img/favicon.png') }}"> --}}
     {{-- <meta name="theme-color" content="#ffffff"> --}}
     <!-- Author Meta -->
     <meta name="author" content="">
